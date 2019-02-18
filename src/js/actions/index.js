cc307218f1feff39e03e57428632a44247eb7cc9
Lawrence Francell <lawrence@twintails.com>
2019-02-18 16:14:27 -0600

    steps/02: add Constants action-types

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,5 +1,7 @@
 // Action Creator: ƒn which returns Object {type: ACTION_CONST, payload }
 
+import { ADD_ITEM } from "Constants/action-types"
+
 export function addItem(payload) {
-  return { type: "ADD_ITEM", payload }
+  return { type: ADD_ITEM, payload }
 };
