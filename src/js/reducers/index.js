fff4afcb5a01cd2080f4f3dd3d88829b28064d5f
Lawrence Francell <lawrence@twintails.com>
2019-03-19 11:50:41 -0500

    steps/03: reduce an action

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,7 +1,12 @@
+import { ADD_ITEM } from '../constants/action-types'
+
 const initialState = {
   items: []
 };
 function rootReducer(state = initialState, action) {
-  return state;
+    if (action.type === ADD_ITEM ){
+      return Object.assign( {}, state, { items: state.items.concat(action.payload) })
+    }
+    return state;
 };
 export default rootReducer;
