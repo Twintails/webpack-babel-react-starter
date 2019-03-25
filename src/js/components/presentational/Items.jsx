83442fc35b532c3792ba3ef1a9049a8e6b89c428
Lawrence Francell <lawrence@twintails.com>
2019-03-25 18:08:14 -0500

    steps/05: Import remove action, add remove button, "connect" Dispatch action with mapDispatchToProps

--- src/js/components/presentational/Items.jsx
+++ src/js/components/presentational/Items.jsx
@@ -1,19 +1,27 @@
 import React from "react"
 import { connect } from "react-redux"
 
-const mapStateToProps = state => {
-  return { items: state.items }
-};
+import { removeItem } from "Actions"
 
-const ConnectedItems = ({ items }) => (
+const mapDispatchToProps = dispatch => ({removeItem: itemID => dispatch(removeItem(itemID))})
+const mapStateToProps = state => ({ items: state.items })
+
+const ConnectedItems = ({ items, removeItem }) => (
   <ul className="list-group list-group-flush col-md-4 offset-md-1">
     {items.map(item => (
       <li className="list-group-item bg-light" key={item.id}>
         {item.title}
+        <button
+          type="button"
+          className="close"
+          onClick={() => {removeItem(item.id)}}
+          aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </li>
     ))}
   </ul>
 );
 
-const Items = connect(mapStateToProps)(ConnectedItems);
+const Items = connect(mapStateToProps, mapDispatchToProps)(ConnectedItems);
 export { Items };
