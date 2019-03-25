d8acfbec6f2602c24a89cbcdbe14b1dc56278e01
Lawrence Francell <lawrence@twintails.com>
2019-03-25 18:04:19 -0500

    steps/05: export new action creator

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,7 +1,11 @@
 // Action Creator: ƒn which returns Object {type: ACTION_CONST, payload }
 
-import { ADD_ITEM } from "Constants/action-types"
+import { ADD_ITEM, REMOVE_ITEM } from "Constants/action-types"
 
 export function addItem(payload) {
   return { type: ADD_ITEM, payload }
+};
+
+export function removeItem(payload) {
+  return { type: REMOVE_ITEM, payload }
 };
