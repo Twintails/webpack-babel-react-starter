8194654d12a904bc3a7e210d07e92caf73debf63
Lawrence Francell <lawrence@twintails.com>
2019-03-25 18:06:59 -0500

    steps/05: Add a reducer for new action type, and fake data

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,11 +1,23 @@
-import { ADD_ITEM } from '../constants/action-types'
+import uuidv1 from "uuid"
+
+import { ADD_ITEM, REMOVE_ITEM } from '../constants/action-types'
 
 const initialState = {
-  items: []
+  items: [
+    { id: uuidv1(), title: "Moo" },
+    { id: uuidv1(), title: "Baa" },
+    { id: uuidv1(), title: "Laa" },
+    { id: uuidv1(), title: "Quack" },
+    { id: uuidv1(), title: "squak" },
+  ]
 };
+
 function rootReducer(state = initialState, action) {
     if (action.type === ADD_ITEM ){
       return { ...state,  items: state.items.concat(action.payload) }
+    }
+    if (action.type === REMOVE_ITEM ){
+      return { ...state, items: state.items.filter(item => item.id !== action.payload) }
     }
     return state;
 };
